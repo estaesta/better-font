9eb26775779f814943ffa1c376e47b9a6f61d02c
Foresta <44913430+estaesta@users.noreply.github.com>
2022-04-14 19:04:33 +0700

    Update Bstation Better Font.user.js
    add blur and increase font size

--- Bstation Better Font.user.js
+++ Bstation Better Font.user.js
@@ -5,7 +5,7 @@
 // @include      https://*.bilibili.tv/*
 // @include      http://*.biliintl.com/*
 // @include      https://*.biliintl.com/*
-// @version      0.2
+// @version      0.3
 // @description  Better font for bstation
 // @author       ddPaud(estaesta)
 // @grant        GM_addStyle
@@ -13,32 +13,33 @@
 
 GM_addStyle ( `
     @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap');
-    .player-mobile .subtitle-item-text {
+    .subtitle-item-text {
         font-family: 'Roboto', sans-serif !important;
-    text-shadow: 1px 1px 0 #000,
-        -1px 1px 0 #000,
-        1px -1px 0 #000,
-        -1px -1px 0 #000,
-        0px 1px 0 #000,
-        0px -1px 0 #000,
-        -1px 0px 0 #000,
-        1px 0px 0 #000,
-        2px 2px 0 #000,
-        -2px 2px 0 #000,
-        2px -2px 0 #000,
-        -2px -2px 0 #000,
-        0px 2px 0 #000,
-        0px -2px 0 #000,
-        -2px 0px 0 #000,
-        2px 0px 0 #000,
-        1px 2px 0 #000,
-        -1px 2px 0 #000,
-        1px -2px 0 #000,
-        -1px -2px 0 #000,
-        2px 1px 0 #000,
-        -2px 1px 0 #000,
-        2px -1px 0 #000,
-        -2px -1px 0 #000 !important;
+    text-shadow: 1px 1px 1px #000,
+        -1px 1px 1px #000,
+        1px -1px 1px #000,
+        -1px -1px 1px #000,
+        0px 1px 1px #000,
+        0px -1px 1px #000,
+        -1px 0px 1px #000,
+        1px 0px 1px #000,
+        2px 2px 1px #000,
+        -2px 2px 1px #000,
+        2px -2px 1px #000,
+        -2px -2px 1px #000,
+        0px 2px 1px #000,
+        0px -2px 1px #000,
+        -2px 0px 1px #000,
+        2px 0px 1px #000,
+        1px 2px 1px #000,
+        -1px 2px 1px #000,
+        1px -2px 1px #000,
+        -1px -2px 1px #000,
+        2px 1px 1px #000,
+        -2px 1px 1px #000,
+        2px -1px 1px #000,
+        -2px -1px 1px #000 !important;
+    font-size: 120%;
     background: transparent !important;
     }
     .player-mobile .subtitle-position.subtitle-position-bc {
