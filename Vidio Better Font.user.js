0d134d9ec59e7bf40522cf5007d9a3229d750173
ddPaud <44913430+estaesta@users.noreply.github.com>
2023-05-06 14:26:15 +0700

    feat: Create Vidio Better Font.user.js
    add users script for vidio.com

---
+++ Vidio Better Font.user.js
@@ -0,0 +1,41 @@
+// ==UserScript==
+// @name         Vidio Better Font
+// @namespace    https://github.com/estaesta
+// @version      0.1
+// @description  Better font for vidio
+// @author       ddPaud(estaesta)
+// @match        https://www.vidio.com/watch/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=vidio.com
+// @grant        GM_addStyle
+// ==/UserScript==
+
+GM_addStyle ( `
+    .video-js .vjs-text-track-display div div div {
+        font-family: inherit !important;
+        text-shadow: 1px 1px 1px #000,
+            -1px 1px 1px #000,
+            1px -1px 1px #000,
+            -1px -1px 1px #000,
+            0px 1px 1px #000,
+            0px -1px 1px #000,
+            -1px 0px 1px #000,
+            1px 0px 1px #000,
+            2px 2px 1px #000,
+            -2px 2px 1px #000,
+            2px -2px 1px #000,
+            -2px -2px 1px #000,
+            0px 2px 1px #000,
+            0px -2px 1px #000,
+            -2px 0px 1px #000,
+            2px 0px 1px #000,
+            1px 2px 1px #000,
+            -1px 2px 1px #000,
+            1px -2px 1px #000,
+            -1px -2px 1px #000,
+            2px 1px 1px #000,
+            -2px 1px 1px #000,
+            2px -1px 1px #000,
+            -2px -1px 1px #000 !important;
+        font-size:95%;
+    }
+` );
